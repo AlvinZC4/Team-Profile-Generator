2d27d6b0bde55769539e1492617ab4cd9dd638f4
Alvin <alvinzcoxiv@gmail.com>
2020-08-01 23:53:48 -0400

    Can now create a one manager object and as many engineer & inter objects as desired using inquire.  Prompts terminate when user is finished creating team.  Finished team is console logged

--- develop/app.js
+++ develop/app.js
@@ -26,7 +26,7 @@ const initApp = () =>
             {
             type: "input",
             name: "id",
-            message: "Enter employee ID number"
+            message: "Enter the employee ID number of the manager"
             },
             {
             type: "input",
@@ -59,6 +59,109 @@ const initApp = () =>
             createIntern()
         }
     })
+}
+const createEngineer = () => {
+    inquirer
+        .prompt([
+            {
+            type: "input",
+            name: "name",
+            message: "Enter the name of the engineer."
+            },
+            {
+            type: "input",
+            name: "id",
+            message: "Enter the employee ID number of the engineer"
+            },
+            {
+            type: "input",
+            name: "email",
+            message: "Enter the engineer's e-mail address"
+            },
+            {
+            type: "input",
+            name: "github",
+            message: "Enter the engineer's GitHub username"
+            },
+            {
+            type: "list",
+            name: "next",
+            message: "Select the role of the next team member",
+            choices: [
+                "Engineer",
+                "Intern",
+                "Finished creating team!"
+                ]
+            }
+    ]).then(function(data) {
+        const newEngineer = new Engineer(data.name, data.id, data.email, data.github)
+
+        teamMembers.push(newEngineer)
+        
+        if (data.next === "Engineer") {
+            createEngineer()
+        }
+        else if (data.next === "Intern") {
+            createIntern()
+        }
+        else {
+            assembleTeam(teamMembers)
+        }
+    })
+}
+const createIntern = () => {
+    inquirer
+        .prompt([
+            {
+            type: "input",
+            name: "name",
+            message: "Enter the name of the intern."
+            },
+            {
+            type: "input",
+            name: "id",
+            message: "Enter the employee ID number of the intern"
+            },
+            {
+            type: "input",
+            name: "email",
+            message: "Enter the intern's e-mail address"
+            },
+            {
+            type: "input",
+            name: "school",
+            message: "Enter the name of the school the intern is currently attending"
+            },
+            {
+            type: "list",
+            name: "next",
+            message: "Select the role of the next team member",
+            choices: [
+                "Engineer",
+                "Intern",
+                "Finished creating team!"
+                ]
+            }
+    ]).then(function(data) {
+        const newIntern = new Intern(data.name, data.id, data.email, data.school)
+
+        teamMembers.push(newIntern)
+        
+        if (data.next === "Engineer") {
+            createEngineer()
+        }
+        else if (data.next === "Intern") {
+            createIntern()
+        }
+        else {
+            assembleTeam(teamMembers)
+        }
+    })
+}
+const assembleTeam = employees => {
+    console.log(employees)
+
+    return
 }
 
 
